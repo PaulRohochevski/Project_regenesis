6df999a9e269468f7de8a93dad68f65253952e32
prahacheuski <paul.rohochevski@gmail.com>
2018-11-28 22:12:01 +0300

    Performed code adjustment.

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-#!C:\Python27\python.exe
-# _*_ coding:utf-8 _*_
-"""
-Main module.
-"""
-from variables import *
-from tickers import *
 import os
-import re
+
+from tickers import *
 
 
 class Concat(object):
@@ -18,8 +12,8 @@ class Concat(object):
         if not re.search("\s$", self.line[number]):
             self.line[number] = self.line[number] + "\n"
 
-    # Append spase
-    def add_spase(self, number):
+    # Append space
+    def add_space(self, number):
         if not re.search("\s$", self.line[number]):
             self.line[number] = self.line[number] + " "
 
@@ -32,59 +26,70 @@ class Concat(object):
                 obj_s.main()
             elif len(self.line) == 2:
                 Concat.add_nl(self, 1)  # Object of class Futures
-                Concat.add_spase(self, 0)
+                Concat.add_space(self, 0)
                 obj_f = Futures(self.line)
                 obj_f.main()
             elif len(self.line) == 3:
                 Concat.add_nl(self, 2)  # Object of class Options
-                Concat.add_spase(self, 0)
-                Concat.add_spase(self, 1)
+                Concat.add_space(self, 0)
+                Concat.add_space(self, 1)
                 obj_o = Options(self.line)
                 obj_o.main()
             else:
-                raise Exception, EXCEPTION_U
+                raise Exception(EXCEPTION_U)
 
 
-# Parse line
-def parser(line):
-    pars_line = re.split(' ', line)
-    var = Concat(pars_line)
-    var.sort()
+class DataParser(object):
+    @staticmethod
+    def parse(row):
+        split_row: str = re.split(' ', row)
+        var = Concat(split_row)
+        var.sort()
 
 
 class Reader(object):
-    # Checking text files
-    def check_text():
+    def __init__(self, file_vec: tuple):
+        self.__validated_file_vec = self.__check_file_vec(file_vec)
+
+    @staticmethod
+    def __check_file_vec(file_vec: tuple):
+        """
+        Check files from vector.
+        :param file_vec: tuple of file paths.
+        :return:
+        """
         file_array = []
-        for _ in [TEXT_FROM_A, TEXT_FROM_B]:
-            if os.path.isfile(_) and os.access(_, os.R_OK):
-                print ("File \"{}\" exists and is readable".format(_))
-                file_array.append(_)
+        for file in file_vec:
+            if os.path.isfile(file) and os.access(file, os.R_OK):
+                print("File \"{}\" exists and readable".format(file))
+                file_array.append(file)
             else:
-                print ("\"{}\" file is missing or is not readable".format(_))
+                print("\"{}\" file is missing or isn't readable".format(file))
         return file_array
 
     # Reading text from files
-    def read_text(file_array):
-        for _file in file_array:
-            text = open(_file, 'rt')
-            for line in text:
-                parser(line)
-            text.close()
+    def read_text(self):
+        for file in self.__validated_file_vec:
+            with open(file, 'rt') as f:
+                for row in f:
+                    DataParser.parse(row)
 
-    if __name__ == '__main__':
-        read_text(check_text())
+
+if __name__ == '__main__':
+    file_vec: tuple = (TEXT_FROM_A, TEXT_FROM_B)
+    reader: Reader = Reader(file_vec)
+    reader.read_text()
 
 # Sort results in final file
-try:
-    _file = open(RESULT_TEXT, 'r+')
-    pos = 0
-    line = _file.readlines()
-    _file.seek(pos)
-    sort_text = sorted(line)
-    for new_line in sort_text:
-        _file.write(new_line)
-        pos = _file.tell()
-    _file.close()
-except IOError:
-    print(EXCEPTION_U)
+# try:
+#     _file = open(RESULT_TEXT, 'r+')
+#     pos = 0
+#     line = _file.readlines()
+#     _file.seek(pos)
+#     sort_text = sorted(line)
+#     for new_line in sort_text:
+#         _file.write(new_line)
+#         pos = _file.tell()
+#     _file.close()
+# except IOError:
+#     print(EXCEPTION_U)
